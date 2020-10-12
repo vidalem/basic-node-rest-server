7cd429fddce838391685477200d9fb95f224061d
Vidal Herrero <vidal@vidalherrero.com>
2020-10-12 13:13:01 +0200

    server intermmediate development

--- server/server.js
+++ server/server.js
@@ -1,80 +1,38 @@
 require('../config/config');
 
 const express = require('express');
+
 const app = express();
 
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.use(require('./routes/user'));
 
-
-app.get('/usuario', function (req, res) {
-
-  res.json('get Usuario');
-
-});
-
-
-app.post('/usuario', function (req, res) {
-
-  let body = req.body;
-
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario"
-    })
-  } else {
-    res.json({
-      persona: body
-    });
-  }
-});
-
-
-app.put('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-
-});
-
-app.delete('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-
-  res.json({
-    id
-  });
-
-});
-
-
-
-
-
-app.get('/', function (req, res) {
-
-  let salida = {
+app.get('/', (req, res) => {
+  const salida = {
     nombre: 'vidal',
     edad: 47,
-    url: req.url
+    url: req.url,
   };
 
   res.send(salida);
-
 });
 
+mongoose.connect('mongodb://localhost:27017/example',
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) throw err;
+
+    // console.log('Database ONLINE');
+  });
+
 app.listen(process.env.PORT, () => {
-  console.log(`Lintening on port ${process.env.PORT}`);
+  // console.log(`Lintening on port ${process.env.PORT}`);
 });
